cddc8a962933ad3deb7453d48b3e235a6546cac9
caio86 <caioluiz86@gmail.com>
2024-09-14 16:41:39 -0300

    create cliente services

---
+++ src/app/shared/services/cliente.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Cliente } from '../models/Cliente';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  api = `${environment.api}/clientes`;
+
+  constructor(private clienteHttp: HttpClient) { }
+
+  inserir(novoCliente: Cliente): Observable<Cliente> {
+    return this.clienteHttp.post<Cliente>(
+      this.api, novoCliente
+    );
+  }
+
+  listar(): Observable<Cliente[]> {
+    return this.clienteHttp.get<Cliente[]>(this.api);
+  }
+
+  listar_paginado(page: number, pageSize: number): Observable<Cliente[]> {
+    return this.clienteHttp.get<Cliente[]>(
+      `${this.api}?page=${page}&pageSize=${pageSize}`
+    )
+  }
+}
