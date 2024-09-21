c166d49a5b81af36d38b5f61d4fe7efec7dcc7c6
caio86 <caioluiz86@gmail.com>
2024-09-21 15:26:46 -0300

    create conta module and service

---
+++ src/app/pages/conta/conta.module.ts
@@ -0,0 +1,16 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  exports: [
+  ],
+})
+export class ContaModule { }
