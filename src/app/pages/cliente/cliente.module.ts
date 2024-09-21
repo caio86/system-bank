51ac217529c843fde76cf3da32c3b8b58c3f8e0d
caio86 <caioluiz86@gmail.com>
2024-09-21 10:17:20 -0300

    generate cliente module
    - add ngx-mask as a dependency

---
+++ src/app/pages/cliente/cliente.module.ts
@@ -0,0 +1,29 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HttpClientModule,
+    MaterialModule,
+    NgxMaskDirective,
+    NgxMaskPipe
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [provideNgxMask()],
+  exports: [],
+})
+export class ClienteModule { }
