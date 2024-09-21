eb799caad5bceda8cf000289f66232386179bb6b
caio86 <caioluiz86@gmail.com>
2024-09-21 14:18:06 -0300

    implement router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListagemClienteComponent } from './pages/cliente/listagem-cliente/listagem-cliente.component';
+import { CadastroClienteComponent } from './pages/cliente/cadastro-cliente/cadastro-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "cliente",
+    children: [
+      {
+        path: "novo",
+        component: CadastroClienteComponent,
+      },
+      {
+        path: "",
+        component: ListagemClienteComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
